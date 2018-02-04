b4d221e2c78e7acef851a7843fd555e1e0c9e823
rihartma <rihartmajr@gmail.com>
2018-02-04 13:45:16 +0100

    some changes of the main structure of the file

--- huffmancoding.py
+++ huffmancoding.py
@@ -1,3 +1,50 @@
+class Compression():
+
+	def __init__(self):
+		pass
+
+	def knots_order(self, array):
+		'''
+		orders knots according to their value from the lowest values
+		have to convert it(from the highest to the lowest values) for uses in this app
+		'''
+		if len(array) <= 1:
+			return array
+
+		pivot_index = len(array)-1
+		array[0], array[-1] = array[-1], array[0]
+		size_border = 0
+
+		for i in range(len(array)-1):
+			if array[i].value<array[pivot_index].value:
+				array[i], array[size_border] = array[size_border], array[i]
+				size_border += 1
+
+		array[pivot_index], array[size_border] = array[size_border], array[pivot_index]
+		pivot_index = size_border
+
+		return(self.knots_order(array[:pivot_index]) + [array[pivot_index]] + self.knots_order(array[pivot_index+1:]))
+
+	def new_knot(self, knots):
+		'''
+		makes a new knot with branches folded from two knots with the lowest values
+		'''
+		if len(knots) < 2:
+			return None
+		new_knot = Knot(knots[-1].value+knots[-2].value)
+		new_knot.left_branch = knots[-1]
+		new_knot.right_branch = knots[-2]
+		del knots[-1:-3:-1]
+		knots.append(new_knot)
+		return knots
+
+	def print_knots(self, knots):
+		for knot in knots:
+			print(knot.value, knot.character)
+		print('\n')
+
+
+
 class Knot():
 
 	def __init__(self, value):
@@ -6,48 +53,13 @@ class Knot():
 		self.right_branch = None
 		self.left_branch = None
 
+
 class Tree():
 
 	def __init__(self):
 		self.root = None
 
-
-def order_knots(array):#orders knots according to their values
-
-    if len(array) <= 1:
-        return array
-
-    pivot_index = len(array)-1
-    array[0], array[-1] = array[-1], array[0]
-    size_border = 0
-    
-    for i in range(len(array)-1):
-        if array[i].value<array[pivot_index].value:
-            array[i], array[size_border] = array[size_border], array[i]
-            size_border += 1
-
-    array[pivot_index], array[size_border] = array[size_border], array[pivot_index]
-    pivot_index = size_border
-
-    return(order_knots(array[:pivot_index]) + [array[pivot_index]] + order_knots(array[pivot_index+1:]))
-
-
-def new_knot(knots):#makes a new knot with branches folded from two knots with the lowest values
-	if len(knots) < 2:
-		return None
-	new_knot = Knot(knots[-1].value+knots[-2].value)
-	new_knot.left_branch = knots[-1]
-	new_knot.right_branch = knots[-2]
-	del knots[-1:-3:-1]
-	knots.append(new_knot)
-	return knots
-
-
-def print_knots(knots):
-	for knot in knots:
-		print(knot.value, knot.character)
-	print('\n')
-
+c = Compression()
 
 chars = ['e','i','a','t','s','p','n']
 frequency = [15,12,10,4,3,13,1]
@@ -58,16 +70,7 @@ for i in range(len(chars)):
 	knots[-1].character = chars[i]
 
 while len(knots) >= 2:
-	knots = order_knots(knots)[::-1]
-	knots = new_knot(knots)
+	knots = c.knots_order(knots)[::-1]
+	knots = c.new_knot(knots)
 
-print_knots(knots)
-
-
-# print_knots(knots)
-# knots = order_knots(knots)[::-1]
-# print_knots(knots)
-# knots = new_knot(knots)
-# print_knots(knots)
-# knots = order_knots(knots)[::-1]
-# print_knots(knots)
+c.print_knots(knots)
