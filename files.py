ac3f7ff481d02e2a8879f720de41c42bfaf86033
rihartma <rihartmajr@gmail.com>
2018-02-05 17:58:09 +0100

    a new method for file loading

--- files.py
+++ files.py
@@ -1,5 +1,12 @@
 class Files():
 
+	def loadFile(self, name):
+		text = ""
+		file = open(name, 'r')
+		text = file.read()
+		file.close()
+		return text
+
 	def saveTable(self, text, name):
 		file = open(name, 'w')
 		file.write(text)
