cc36b966c3f200887751c1c0e58dcd014cb8646f
rihartma <rihartmajr@gmail.com>
2018-02-08 13:45:58 +0100

    saving the decompressed file

--- decompress.py
+++ decompress.py
@@ -2,12 +2,20 @@ from sys import argv
 from files import Files
 from huffmancoding import Compression, Decompression
 
+try:
+	f = Files()
+	filetodecomp = argv[1]
+	dfilename = argv[2]
+	text = f.loadFile(filetodecomp)
+	table = f.loadTable(filetodecomp[0:-4]+"_table.txt")
+	d = Decompression()
+	binar = d.makeBinar(text)
+	decomptext = d.decompText(binar, table)
+	f.saveTable(decomptext, dfilename)
 
-f = Files()
-filetodecomp = argv[1]
-text = f.loadFile(filetodecomp)
-table = f.loadTable(filetodecomp[0:-4]+"_table.txt")
-d = Decompression()
-binar = d.makeBinar(text)
-decomptext = d.decompText(binar, table)
-print(decomptext)
+except IndexError:
+	print("Usage: python3 decompress.py <name-of-the-compressed-file> <decompressed_filename>")
+
+except FileNotFoundError:
+	print("Not valid filenames!")
+	print("Usage: python3 decompress.py <name-of-the-compressed-file> <decompressed_filename>")
